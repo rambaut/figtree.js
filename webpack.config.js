5cf1e950885882a23c1b94d337adef0a226a766a
Andrew Rambaut <rambaut@gmail.com>
2018-08-05 15:13:48 +0100

    Updating webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src', 'figtree.js'),
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'js'),
     filename: 'figtree.js'
   },
   externals: [
