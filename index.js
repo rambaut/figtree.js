23d6bd2cd40319b6ef1b2101a12830116653f77d
JT <mccrone@umich.edu>
2019-02-20 10:58:28 +0000

    adding index.js for install in other projects

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Tree, Type } from "src/tree.js";
+import { rectangularLayout,transmissionLayout } from "src/layouts.js";
+import {Figtree} from "src/figtree.js";
+// automate with gulp/grunt
+
+export{
+    Tree,
+    Type,
+    rectangularLayout,
+    transmissionLayout,
+    Figtree
+}
