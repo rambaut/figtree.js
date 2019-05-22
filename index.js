c7b2e9d1e2787125145f5eecd85b2677335bf313
JT <mccrone@umich.edu>
2019-05-22 12:34:37 +0100

    updates forgotten exports

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { Tree, Type } from "./src/tree.js";
 import { Layout } from "./src/layout.js";
 import { RectangularLayout } from "./src/rectangularLayout.js";
+import {transmissionLayout} from "./src/transmissionLayout.js"
+import {ArcLayout} from "./src/arcLayout.js"
 import {Bauble,CircleBauble,RectangularBauble } from "./src/bauble.js"
 import {FigTree} from "./src/figtree.js";
 import {Graph} from "./src/Graph.js"
 
 
-export{Tree,Type,RectangularLayout,transmissionLayout, FigTree, Bauble,CircleBauble,RectangularBauble,Layout, Graph};
+export{Tree,Type,ArcLayout,RectangularLayout,transmissionLayout, FigTree, Bauble,CircleBauble,RectangularBauble,Layout, Graph};
