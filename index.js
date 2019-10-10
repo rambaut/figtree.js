bb6f57a3508e5612d301c22057bce85eb2068a03
JT <mccrone@umich.edu>
2019-10-10 13:45:56 +0100

    merging in rough branch and minification

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {TransmissionLayout} from "./src/layout/transmissionlayout.js"
 import {ExplodedLayout} from "./src/layout/explodedlayout.js"
 import {Bauble,CircleBauble,RectangularBauble,RoughCircleBauble } from "./src/figtree/bauble.js"
 import {FigTree} from "./src/figtree/figtree.js";
-import {RootToTipPlot} from './src/roottotipplot'
+import {RootToTipPlot} from './src/layout/roottotipplot'
 import {RoughFigTree} from './src/figtree/roughfigtree.js'
 
 export{Tree,Type,RootToTipPlot,RectangularLayout,TransmissionLayout, ExplodedLayout, FigTree,RoughFigTree, Bauble,CircleBauble,RoughCircleBauble,RectangularBauble};
