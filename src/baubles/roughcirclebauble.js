21a0903a785f7b77e7ac3e38760a3a0c03b46dca
jtmccr1 <mccronejt@gmail.com>
2019-10-11 23:35:03 +0100

    passes data down to rough paths

--- src/baubles/roughcirclebauble.js
+++ src/baubles/roughcirclebauble.js
@@ -40,43 +40,40 @@ export class RoughCircleBauble extends Bauble {
     updateShapes(selection, border = 0) {
 
 
-        const newPaths = [...roughFactory.circle(0, 0, this.settings.radius + border, this.settings).childNodes].map(d => d.getAttribute("d"));
-        const pathNames = newPaths.length === 2 ? ["roughFill", "roughStroke"] : ["roughStroke"];
+        const newPaths = [...roughFactory.circle(0, 0, this.settings.radius + border, this.settings).childNodes]
+            .map(d => d.getAttribute("d")).reverse(); // this puts the "stroke" before the fill so if there is no fill we just never hit it below
+        const pathNames = ["roughStroke","roughFill"] ;
         return selection.selectAll("path")
-            .data(pathNames, d => d)
+            .data(d =>[d,d].slice(0,newPaths.length))
             .join(
                 enter => enter
                     .append("path")
                     .attr("d", (d, i) => newPaths[i])
                     .attr("class", d => `${d} node-shape rough`)
-                    .attrs((d, i, n) => {
-                        const attributes = this.settings.attrs[d];
+                    .attrs((vertex, i) => {
+                        const attributes = this.settings.attrs[pathNames[i]];
                         return Object.keys(attributes).reduce((acc, curr) => {
-                            const vertex = d3.select(n[i].parentNode).datum(); // the vertex data is assigned to the group
                             return {...acc, [curr]: attributes[curr](vertex)}
                         }, {})
                     })
-                    .styles((d, i, n) => {
-                        const styles = this.settings.styles[d];
+                    .styles((vertex, i, n) => {
+                        const styles = this.settings.styles[pathNames[i]];
                         return Object.keys(styles).reduce((acc, curr) => {
-                            const vertex = d3.select(n[i].parentNode).datum(); // the vertex data is assigned to the group
                             return {...acc, [curr]: styles[curr](vertex)}
                         }, {})
                     }),
                 update => update
                     .call(update => update.transition()
                         .attr("d", (d, i) => newPaths[i])
-                        .attrs((d, i, n) => {
-                            const attributes = this.settings.attrs[d];
+                        .attrs((vertex, i) => {
+                            const attributes = this.settings.attrs[pathNames[i]];
                             return Object.keys(attributes).reduce((acc, curr) => {
-                                const vertex = d3.select(n[i].parentNode).datum(); // the vertex data is assigned to the group
                                 return {...acc, [curr]: attributes[curr](vertex)}
                             }, {})
                         })
-                        .styles((d, i, n) => {
-                            const styles = this.settings.styles[d];
+                        .styles((vertex, i) => {
+                            const styles = this.settings.styles[pathNames[i]];
                             return Object.keys(styles).reduce((acc, curr) => {
-                                const vertex = d3.select(n[i].parentNode).datum(); // the vertex data is assigned to the group
                                 return {...acc, [curr]: styles[curr](vertex)}
                             }, {})
                         }),
